6c333709e8ba123b843e276d58d6913dc3c28110
s3xmaxman <badside3919@gmail.com>
2024-08-19 18:07:59 +0900

    update 7 files

--- next-frontend/app/login/page.tsx
+++ next-frontend/app/login/page.tsx
@@ -1,10 +1,17 @@
 "use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useAuth } from "@/components/authProvider";
+
 const LOGIN_URL = "/api/login/";
 
 export default function Page() {
   const auth = useAuth();
-
   async function handleSubmit(event: any) {
     event.preventDefault();
     console.log(event, event.target);
@@ -19,35 +26,70 @@ export default function Page() {
       body: jsonData,
     };
     const response = await fetch(LOGIN_URL, requestOptions);
+    let data = {};
+    try {
+      data = await response.json();
+    } catch (error) {}
     // const data = await response.json()
     if (response.ok) {
       console.log("logged in");
-      auth.login();
+      auth.login(data?.username);
+    } else {
+      console.log(await response.json());
     }
   }
-
   return (
-    <div className="h-[95vh]">
-      <div className="max-w-md mx-auto py-5">
-        <h1>Login Here</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            required
-            name="username"
-            placeholder="Your Username"
-            className="text-black"
-          />
-          <input
-            type="password"
-            required
-            name="password"
-            placeholder="Your password"
-            className="text-black"
-          />
-
-          <button type="submit">Login</button>
-        </form>
+    <div className="w-full lg:grid lg:min-h-[85vh]  lg:grid-cols-2 xl:min-h-[90vh]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <form onSubmit={handleSubmit}>
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="username"
+                  name="username"
+                  placeholder="Your username"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <Link href="/forgot-password" className="hidden">
+                    Forgot your password?
+                  </Link>
+                </div>
+                <Input id="password" name="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </form>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </div>
   );
