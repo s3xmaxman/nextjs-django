66b45ad487632444d189e1e1a34bec1a3601b59e
s3xmaxman <badside3919@gmail.com>
2024-08-19 11:37:07 +0900

    update route.tsx

--- next-frontend/app/api/login/route.tsx
+++ next-frontend/app/api/login/route.tsx
@@ -1,4 +1,31 @@
 "use server";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {}
+const DJANGO_API_LOGIN_URL = "http://127.0.0.1:8001/api/token/pair";
+
+export async function POST(request: Request) {
+  const requestData = await request.json();
+
+  const jsonData = JSON.stringify(requestData);
+
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: jsonData,
+  };
+
+  const response = await fetch(DJANGO_API_LOGIN_URL, requestOptions);
+  const responseData = await response.json();
+
+  if (response.ok) {
+    return NextResponse.json(
+      { loggedIn: true, ...responseData },
+      { status: 200 }
+    );
+  }
+
+  return NextResponse.json(
+    { loggedIn: false, ...responseData },
+    { status: 400 }
+  );
+}
